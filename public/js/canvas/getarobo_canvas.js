a32f5ba05819df9284db25569b7e19f782c9deb1
gene <thefightingbee@gmail.com>
2019-04-22 16:53:29 -0400

    hmm

--- public/js/canvas/getarobo_canvas.js
+++ public/js/canvas/getarobo_canvas.js
@@ -1,14 +1,2 @@
 
 		var ctx = document.querySelector('canvas').getContext('2d');
-
-
-		window.addEventListener('resize', resizeCanvas, false);
-
-
-		function resizeCanvas() {
-			var w = document.body.offsetWidth,
-			    h = document.body.offsetHeight;
-			ctx.canvas.width  = w;
-			ctx.canvas.height = h;
-
-		}
