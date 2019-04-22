8ca68e65d5886b4d787b89096f095c7fb795730c
gene <thefightingbee@gmail.com>
2019-04-22 16:36:13 -0400

    mssing js

---
+++ public/js/canvas/getarobo_canvas.js
@@ -0,0 +1,35 @@
+
+		var ctx = document.querySelector('canvas').getContext('2d'),
+		    fpsout = document.querySelector('#fps');
+
+
+		resizeCanvas();
+		window.addEventListener('resize', resizeCanvas, false);
+		setInterval(drawFrame, 1000 / 60);
+
+		function resizeCanvas() {
+			var w = document.body.offsetWidth,
+			    h = document.body.offsetHeight;
+			ctx.canvas.width  = w;
+			ctx.canvas.height = h;
+			ctx.translate(w/2,h/2);
+			ctx.strokeStyle = 'red';
+			ctx.fillStyle = 'black';
+		}
+
+
+		var lastUpdate = new Date, fps=30;
+
+		function drawFrame() {
+			var now = new Date;
+			var dt = (now - lastUpdate) / 1000;
+			if (dt==0 || isNaN(dt)) return;
+
+			ctx.save();
+			ctx.setTransform(1,0,0,1,0,0);
+			ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+			ctx.restore();
+
+			lastUpdate = now;
+			fps += (1/dt - fps) / 10;
+		}
