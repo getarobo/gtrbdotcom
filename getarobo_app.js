1adcdadca4f369e20816203fea174d462ce2d731
gene <thefightingbee@gmail.com>
2019-04-22 16:25:11 -0400

    simple webgl

---
+++ getarobo_app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+
+// express
+var app = express();
+var port = 3001;
+
+// view egeine
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade')
+app.use( express.static(path.join(__dirname, 'public')));
+
+//var indexPage = require('./routes/index');
+//app.use("/", indexPage);
+
+var serveStatic = require('serve-static');
+
+//app.use("/", serveStatic( path.join(__dirname, 'palmtree')));
+app.use("/", serveStatic( path.join(__dirname, 'elemij_canvas')));
+//app.use("/canvas", serveStatic( path.join(__dirname, 'elemij_canvas')));
+
+
+
+app.listen(port);
