845f3d6438bb25c2980840f46ed1fe03605c819e
gene <thefightingbee@gmail.com>
2019-04-22 16:51:59 -0400

    hmm

--- public/js/canvas/getarobo_canvas.js
+++ public/js/canvas/getarobo_canvas.js
@@ -5,7 +5,7 @@
 
 		resizeCanvas();
 		window.addEventListener('resize', resizeCanvas, false);
-		setInterval(drawFrame, 1000 / 60);
+
 
 		function resizeCanvas() {
 			var w = document.body.offsetWidth,
@@ -13,18 +13,4 @@
 			ctx.canvas.width  = w;
 			ctx.canvas.height = h;
 
-		}
-
-
-		var lastUpdate = new Date, fps=30;
-
-		function drawFrame() {
-			var now = new Date;
-			var dt = (now - lastUpdate) / 1000;
-			if (dt==0 || isNaN(dt)) return;
-
-
-
-			lastUpdate = now;
-			fps += (1/dt - fps) / 10;
 		}
