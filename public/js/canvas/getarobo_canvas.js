2f5d61d370796cb0f825c1d521bdc6b552c53884
gene <thefightingbee@gmail.com>
2019-04-23 17:37:35 -0400

    fps

--- public/js/canvas/getarobo_canvas.js
+++ public/js/canvas/getarobo_canvas.js
@@ -2,4 +2,5 @@
 
 var canvas = document.createElement("canvas");
 canvas.setAttribute("data-fragment-url", "../shader/distance.frag");
+ document.body.appendChild(canvas);
 var sandbox = new GlslCanvas(canvas);
