57393bca192aba455bbb1753dd1df79cf2820da6
gene <thefightingbee@gmail.com>
2019-04-23 17:32:12 -0400

    fps

--- public/js/canvas/getarobo_canvas.js
+++ public/js/canvas/getarobo_canvas.js
@@ -1,20 +1,6 @@
-var lastCalledTime;
-var fps;
 
 
-window.requestAnimationFrame(requestAnimFrame);
-console.log("HI:"+0);
+var canvas = document.createElement("canvas");
+var sandbox = new GlslCanvas(canvas);
 
-function requestAnimFrame() {
-
-  if(!lastCalledTime) {
-     lastCalledTime = Date.now();
-     fps = 0;
-     return;
-  }
-  delta = (Date.now() - lastCalledTime)/1000;
-  lastCalledTime = Date.now();
-  fps = 1/delta;
-	console.log("fps:"+fps);
-	document.getElementById("fps").innerHTML = ""+fps;
-}
+sandbox.load("../shader/lava-lamp.frag");
